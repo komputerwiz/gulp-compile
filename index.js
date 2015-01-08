a415f4c40ab8ca573802ff53bfb8d40b0d2e2d7d
Matthew Barry <mbarry@tamu.edu>
2015-01-07 21:07:00 -0600

    initial plugin implementation
    https://bugs.citd.tamu.edu/show_bug.cgi?id=* master

---
+++ index.js
@@ -0,0 +1,41 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+var _ = require('lodash');
+
+
+module.exports = function (options) {
+
+    // default options
+    var opts = _.defaults(_.clone(options) || {}, {
+        // use lodash/underscore compiler by default
+        compiler: _.template
+    });
+
+    return through.obj(function (file, encoding, next) {
+        if (file.isNull()) {
+            return next(null, file);
+        }
+
+        if (file.isStream()) {
+            // emit error, but keep processing
+            this.emit('error', new gutil.PluginError('compile', 'Streaming not supported'));
+            return next();
+        }
+
+        if (file.isBuffer()) {
+            // compile template source into JS function
+            var template = file.contents.toString();
+            var compiled = opts.compiler(template);
+
+            // transform compiled JS function into source code string
+            file.contents = new Buffer(compiled.toString());
+
+            // rename file to .js extension
+            file.path = gutil.replaceExtension(file.path, '.js');
+        }
+
+        // pass (potentially) compiled template along
+        next(null, file);
+    });
+
+};
